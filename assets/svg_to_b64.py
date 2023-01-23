7e6f86e57440d7f407e7ffd74e3f352e1bb77351
Naomi Tesla <naomitesla@protonmail.com>
2023-01-22 21:07:31 -0500

    made code a bit prettier

--- assets/svg_to_b64.py
+++ assets/svg_to_b64.py
@@ -8,25 +8,23 @@ py_dir = os.path.dirname(py_path)
 os.chdir(py_dir + "/svgs/")
 
 
-
 def cap(file=''):
-	side = abs(50 - len(file))//2
-	if len(file) > 0:
-		cap = "<" + "="*side + file + "="*side + ">" + "\n"
-	else:
-		cap = "\n" + "<" + "="*side + file + "="*side + ">" + "\n"*2
-	return cap
+    side = abs(50 - len(file))//2
+    if len(file) > 0:
+        return "<" + "="*side + file + "="*side + ">" + "\n"
+    return "\n" + "<" + "="*side + file + "="*side + ">" + "\n"*2
 
 
 img_dict = {}
 
 for file in glob.glob("*.svg"):
-	with open(file, "rb") as image:
-		img_dict[file] = "data:image/svg+xml;charset=utf-8;base64, " + base64.b64encode(image.read()).decode("utf-8")
+    with open(file, "rb") as image:
+        img_dict[file] = "data:image/svg+xml;charset=utf-8;base64, " + \
+            base64.b64encode(image.read()).decode("utf-8")
 
 os.chdir(py_dir)
 with open("svgs_b64_encoded_output.txt", 'w') as f:
-	for file, image in img_dict.items():
-		f.write(cap(file))
-		f.write(image)
-		f.write(cap())
+    for file, image in img_dict.items():
+        f.write(cap(file))
+        f.write(image)
+        f.write(cap())
